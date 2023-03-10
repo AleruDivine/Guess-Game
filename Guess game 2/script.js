728934592333a99a792586fd0b4f75de34888085
AleruDivine <alerudivine@gmail.com>
2023-03-10 17:53:59 +0100

    modified

--- Guess game 2/script.js
+++ Guess game 2/script.js
@@ -1,3 +1,11 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
- 
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'correct number!';
+// console.log(document.querySelector('.message').textContent);
+document.querySelector('.check').addEventListener
+//arguments//
+('click',function(){
+  const guess = (document.querySelector('.guess').value);
+  console.log(typeof guess);
+});
