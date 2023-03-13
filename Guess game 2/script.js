2d3a7c264cbf46329c26541d3cb5e24f2a000dd1
AleruDivine <alerudivine@gmail.com>
2023-03-13 23:56:56 +0100

    further commit

--- Guess game 2/script.js
+++ Guess game 2/script.js
@@ -6,7 +6,9 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-// document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
+
+ // document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -20,6 +22,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = '?';
 
     document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#855e84';
+    document.querySelector('.number').style.width = '15rem';
   });
 
   if (!guess) {
@@ -29,7 +34,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
-  } else if (guess > secretNumber) {
+    if (score > highScore)
+{highScore = score}
+document.querySelector('.highscore').textContent = highScore;
+
+
+} else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too high!';
       score = score - 1;
